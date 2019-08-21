7f1ecdee43c23b260050f9832b24767d72f16b34
Zzzoomprism <s96k92@gmail.com>
2019-08-21 11:44:46 +0300

    Updates

---
+++ precache-manifest.8795861c039ff8807970fd616c3716c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527e73366fa81323f9ee8ae0e6845503",
+    "url": "/reactjs-template1/index.html"
+  },
+  {
+    "revision": "17b221fe5334ebd5c3f4",
+    "url": "/reactjs-template1/static/css/main.146037fc.chunk.css"
+  },
+  {
+    "revision": "b48d75fb23e7cde4db49",
+    "url": "/reactjs-template1/static/js/2.f4bf9465.chunk.js"
+  },
+  {
+    "revision": "17b221fe5334ebd5c3f4",
+    "url": "/reactjs-template1/static/js/main.61ddd6db.chunk.js"
+  },
+  {
+    "revision": "822c4ebb7a0321d30958",
+    "url": "/reactjs-template1/static/js/runtime~main.6468ed0d.js"
+  }
+]);
